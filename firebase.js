41598d18c397192a9d30455433be165d1dd1df29
abidahammed <54079464+abidplabon@users.noreply.github.com>
2022-01-22 20:32:34 +0600

    Development 3

---
+++ firebase.js
@@ -0,0 +1,9 @@
+import firebase from "firebase";
+const firebaseConfig = {
+    apiKey: "AIzaSyDEAxLb0ISSqdn-ek5dlqcOTtSuE6w0Z3Q",
+    authDomain: "clone-2f24c.firebaseapp.com",
+    projectId: "clone-2f24c",
+    storageBucket: "clone-2f24c.appspot.com",
+    messagingSenderId: "308535426395",
+    appId: "1:308535426395:web:992b18e38c227e6e846e7e"
+  };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import {MenuIcon,SearchIcon,ShoppingCartIcon} from "@heroicons/react/outline";
+import {signIn,signOut,useSession} from "next-auth/client";
 
 function Header() {
   return <header>
@@ -22,7 +23,7 @@ function Header() {
         <SearchIcon className='h-12 p-4'/>
     </div>
     <div className='text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap'>
-      <div className='link'>
+      <div onClick={signIn} className='link'>
         <p>Hello there Abid</p>
         <p className='font-extrabold md:text-sm'>Account & Lists</p>
       </div>
